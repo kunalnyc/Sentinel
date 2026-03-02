af4362d64fe7560b7526a03630da81ac1c3beac9
Kunal <kunalkmr9717@outlook.com>
2026-03-02 10:39:34 +0000

    feat: 64-bit SentinelOS with graphics and Forerunner UI working

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -8,46 +8,53 @@ mb_start:
     dd 0xE85250D6
     dd 0
     dd mb_end - mb_start
-    dd -(0xE85250D6 + 0 + (mb_end - mb_start))
+    dd 0x100000000 - (0xE85250D6 + 0 + (mb_end - mb_start))
     align 8
     dw 0
     dw 0
     dd 8
 mb_end:
 
-section .bss
-align 16
-resb 16384
-stack_top:
-
 section .text
 global _start
 _start:
-    mov esp, stack_top
+    mov esp, 0x90000
 
-    ; Save magic and mb_ptr before anything
+    ; Save magic and mb_ptr
     mov [saved_magic], eax
     mov [saved_mbptr], ebx
+
+    ; We need to switch to real mode to call BIOS for VESA
+    ; But we're already in protected mode from GRUB
+    ; So instead - write VBE mode info to known address
+    ; and let kernel use it
 
     ; Clear page tables
     mov edi, 0x1000
     xor eax, eax
-    mov ecx, 0x3000 / 4
+    mov ecx, 0x4000 / 4
     rep stosd
 
-    ; PML4
+    ; PML4 at 0x1000 - map first 4GB
     mov dword [0x1000], 0x2003
     mov dword [0x1004], 0
 
-    ; PDPT
+    ; PDPT at 0x2000 - map first 1GB
     mov dword [0x2000], 0x3003
     mov dword [0x2004], 0
 
-    ; PD
-    mov dword [0x3000], 0x0083
-    mov dword [0x3004], 0
+    ; PD at 0x3000 - map first 512MB with 2MB pages
+    mov eax, 0x0083
+    mov ecx, 256
+    mov edi, 0x3000
+.map_pd:
+    mov dword [edi], eax
+    mov dword [edi+4], 0
+    add eax, 0x200000
+    add edi, 8
+    loop .map_pd
 
-    ; Load CR3
+    ; Load PML4
     mov eax, 0x1000
     mov cr3, eax
 
@@ -95,7 +102,6 @@ lmode:
     mov rsp, 0x90000
     xor rbp, rbp
 
-    ; Load arguments for kernel_main
     xor rdi, rdi
     xor rsi, rsi
     mov edi, [rel saved_magic]
